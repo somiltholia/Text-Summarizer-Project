330ddbbaf63fe4c8a0554b83100fd54efd522c8b
Somil Jain <somiltholia01@gmail.com>
2024-10-23 11:47:39 +0530

    requirement installation

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 
-with open("README.md","r", encoding="utf-8") as f:
-    long_description=f.read()
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
 
 
 __version__ = "0.0.0"
@@ -14,17 +14,17 @@ AUTHOR_EMAIL = "somiltholia01@gmail.com"
 
 
 setuptools.setup(
-    name=SRC_REPO,  # Name of the package
-    version=__version__,           # Initial version
-    author=AUTHOR_USER_NAME,        # Your name
-    author_email=AUTHOR_EMAIL,  # Your email
-    description="A small python package for CNN app",  # Short description
-    long_description=long_description,  # Long description from README
-    long_description_content="text/markdown",  # Markdown format for long description
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",  # URL of the project repo
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
     package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),  # Automatically find and include all packages in the project
+    packages=setuptools.find_packages(where="src")
 )
--- template.py
+++ template.py
@@ -10,7 +10,7 @@ project_name = "textSummarizer"
 list_of_files = [
     ".github/workflows/.gitkeep",
     f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/conponents/__init__.py",
     f"src/{project_name}/utils/__init__.py",
     f"src/{project_name}/utils/common.py",
     f"src/{project_name}/logging/__init__.py",
@@ -27,21 +27,25 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "test.py"
+
 ]
+
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
-    
-    if filedir !="":
-        os.makedirs(filedir, exist_ok=True)
-        logging.info(f"Creating Directory: {filedir} for the file {filename}")
 
-    if(not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory:{filedir} for the file {filename}")
+
+    
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath,'w') as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
 
+
+    
     else:
-        logging.info(f"{filename} already exists")
+        logging.info(f"{filename} is already exists")
